041072994bec71a60685583678e78d47a537dd31
Josival José da Silva <silva.josival@yahoo.com>
2023-09-01 01:40:02 -0300

    Montando uma classe classe própria para manipular os dados da API e configurando o layout da página

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,24 @@
-function convertPokemonTypesToLi(pokemonTypes) {
-  return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
 function convertPokemonToLi(pokemon) {
   return `
-    <li class="pokemon">
-      <span class="number">#00${pokemon.order}</span>
+    <li class="pokemon ${pokemon.type}">
+      <span class="number">#00${pokemon.number}</span>
       <span class="name">${pokemon.name}</span>
 
       <div class="detail">
         <ol class="types">
-          ${convertPokemonTypesToLi(pokemon.types).join('')}
+         ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
         </ol>
 
-        <img src=${pokemon.sprites.other.dream_world.front_default} alt="${pokemon.name}">
+        <img src=${pokemon.photo}
+              alt="${pokemon.name}">
       </div>
     </li>
 `
 }
 
-const pokemonList = document.getElementById('pokemonList')
+const pokemonList = document.getElementById("pokemonList")
 
   pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
   })
